5f9c0ebedde79c082203e12523ab3f6cb6657931
OBrien McQuade <62272369+McFlyin619@users.noreply.github.com>
2020-04-18 10:26:45 -0700

    Basics
    learning the basics

---
+++ script.js
@@ -0,0 +1,31 @@
+// let num1=5;
+// let num2=6;
+
+// let result;
+
+// result=num1+num2;
+// result=result*num1;
+
+// console.log(result);
+// console.log(num1+num2);
+
+// result= Math.round(2.55); 
+// result= Math.floor(2.99); 
+// result= Math.ceil(2.1);
+// result= Math.sqrt(144);
+// result= Math.min(10,2,5,8,7,6);
+// result= Math.max(10,2,5,8,7,6);
+
+
+// console.log(result);
+
+let num1=50,
+    num2="10",
+    num3="two",
+    num4="20.20";
+
+let data;
+
+console.log(num1+Number(num2));
+console.log(num1+parseFloat(num4));
+
---
+++ script2.js
@@ -0,0 +1,55 @@
+// // variable types: var 
+
+// var name="Daniel";
+// console.log(name);
+
+// var name="Rene";
+// console.log(name);
+
+// // let
+
+// let clientName = "Gareth";
+// clientName = "Joel";
+
+// // const
+
+// const client = "Christian";
+
+// String
+
+// let band='Metallica', song="Enter Sandman";
+
+// let name;
+
+// console.log(band+song);
+
+// name = name.concat(' ',"is great!");
+
+// console.log(name.length);
+
+// name= name.toUpperCase();
+
+// console.log(name);
+
+// name= name.toLowerCase();
+
+// console.log(name);
+
+// console.log(`${band} ${song}`);
+
+// let name;
+// let activity= "Im Learning JavaScript in FSDI";
+
+// name=activity.includes('JavaScript');
+// name=activity.split(' ');
+
+// let interest= "Read, walk, listen music, programming learning";
+
+// name=interest.split(',');
+
+// email varification
+// let email="obrien@gmail.com";
+// name=email.includes("@");
+// console.log(name);
+// console.log(activity.repeat(5));
+
